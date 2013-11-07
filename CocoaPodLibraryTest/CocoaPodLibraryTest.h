27ecd8c4974899b6e767f9133b25a942323c7a70
Michael Anderson <michael.anderson@runwithrobots.com>
2013-11-07 14:37:01 +0800

    Initial Commit

---
+++ CocoaPodLibraryTest/CocoaPodLibraryTest.h
@@ -0,0 +1,13 @@
+//
+//  CocoaPodLibraryTest.h
+//  CocoaPodLibraryTest
+//
+//  Created by Michael Anderson on 7/11/13.
+//  Copyright (c) 2013 Michael Anderson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaPodLibraryTest : NSObject
+
+@end
---
+++ CocoaPodLibraryTest/CocoaPodLibraryTest.m
@@ -0,0 +1,13 @@
+//
+//  CocoaPodLibraryTest.m
+//  CocoaPodLibraryTest
+//
+//  Created by Michael Anderson on 7/11/13.
+//  Copyright (c) 2013 Michael Anderson. All rights reserved.
+//
+
+#import "CocoaPodLibraryTest.h"
+
+@implementation CocoaPodLibraryTest
+
+@end
